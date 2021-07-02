1ba91d93903796af5f47793ad0e60cacc627da6a
livanska <ivanskalilia16@gmail.com>
2021-07-02 12:39:32 +0300

    Created animated sidebar

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,23 @@
+import Header from "./Header/Header"
+import { Sidebar } from "./Sidebar/Sidebar"
+import css from './Layout.module.scss'
+import React, { ReactNode, useState } from "react"
+
+interface LayoutProps {
+    children?: React.ReactNode;
+}
+export const Layout = ({children}:LayoutProps ) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    return (
+        <div className={css.page}>
+            
+            <Sidebar isOpen={isSidebarOpen}
+                setIsOpen={setIsSidebarOpen} />
+                <Header  setIsOpenSidebar={setIsSidebarOpen} />
+            <main className={css.page__content}>{children}</main>
+            </div>
+    )
+
+}
+
+export default Layout
