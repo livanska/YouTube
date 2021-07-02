b82d030db9ee8f02d69d829930a0ec89690b456d
livanska <ivanskalilia16@gmail.com>
2021-07-02 16:44:20 +0300

    Added VideoCard component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,23 @@
+import VideoCard from '../../components/VideoCard/VideoCard'
+import css from './Home.module.scss'
 const Home = () => {
     return(
-        <div>Home route</div>
+        <div className={css.videoGrid}>
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+
+
+        </div>
     )
 }
 export default Home;
