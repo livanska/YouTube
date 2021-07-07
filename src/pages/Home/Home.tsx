6ed9968db122d3c36f7b81f58e797815dc572233
livanska <ivanskalilia16@gmail.com>
2021-07-07 12:09:55 +0300

    Testing deploy to firebase script #8

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -13,8 +13,7 @@ const Home = () => {
             <VideoCard />
             <VideoCard />
             <VideoCard />
-            <VideoCard />
-            <VideoCard />
+
 
         </div>
     )
