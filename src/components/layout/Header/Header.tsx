88093ca4776a0bc558b7f9f8b182bba999eea18d
livanska <ivanskalilia16@gmail.com>
2021-07-08 16:50:53 +0300

    Implemented basic auth

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,13 +1,52 @@
 
 import css from './Header.module.scss';
 import Logo from '../../../assets/YouTube_logo.png';
+import { ReactElement, useEffect, useState } from 'react';
+import { API_KEY, CLIENT_ID, DISCOVERY_URL, SCOPES } from '../../../constants'
+import React from 'react';
 
-
+var GoogleAuth: gapi.auth2.GoogleAuth;
 interface HeaderProps {
     setIsOpenSidebar(value: boolean): void;
 }
 
 export const Header = ({ setIsOpenSidebar }: HeaderProps) => {
+
+    const [signedIn, setSignedIn] = useState(false)
+    const [user,setUser] = useState<gapi.auth2.BasicProfile>()
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "https://apis.google.com/js/client.js";
+        script.onload = () => {
+            gapi.load('client:auth2', () => { initClient() });
+        };
+        document.body.appendChild(script);
+    }, [])
+
+    const initClient = () => {
+        window.gapi.client.load('youtube', 'v3', () => {
+            gapi.client.init({
+                'apiKey': API_KEY,
+                'clientId': CLIENT_ID,
+                'scope': SCOPES,
+                'discoveryDocs': DISCOVERY_URL
+            })
+        });
+
+    }
+    async function handleLogIn() {
+        GoogleAuth = gapi.auth2.getAuthInstance();
+        await GoogleAuth.signIn();
+        GoogleAuth.isSignedIn && setSignedIn(true)
+        setUser(GoogleAuth.currentUser.get().getBasicProfile())
+    }
+    async function handleLogOut() {
+        
+        await GoogleAuth.signOut().then(function () {
+        });
+        !GoogleAuth.currentUser.get().isSignedIn() && setSignedIn(false)
+        window.location.reload();
+    }
 
     return (
         <div className={css.header}>
@@ -29,9 +68,12 @@ export const Header = ({ setIsOpenSidebar }: HeaderProps) => {
                     <span className="fas fa-video"></span>
                     <span className="fas fa-th"></span>
                     <span className="fas fa-bell"></span>
-                    <img  alt='Avatar'
-                        src='https://yt3.ggpht.com/ytc/AAUvwniNcj7dsdmooO6x7NoI01OQRlM1Rk9PqYue1LN7=s68-c-k-c0x00ffffff-no-rj' />
-                </div>
+                    {user ? <React.Fragment><img alt='Avatar'
+                        src={user.getImageUrl()} />
+                    <button onClick={handleLogOut}>LogOut</button></React.Fragment>
+                    :<button onClick={handleLogIn}>LogIn</button>
+                   }
+                    </div>
             </div>
         </div>
     )
